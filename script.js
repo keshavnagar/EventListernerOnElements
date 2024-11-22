8b5c93e33d293fd4f7ce529cc9344dede987203b
Keshav Nagar <keshavnagar887@gmail.com>
2024-11-22 15:14:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let p = document.querySelector("p");
+p.addEventListener("click",function(){
+    console.log("clicked on paragraph")
+})
+
+
+let box = document.querySelector(".box")
+box.addEventListener("mouseenter",function(){
+    console.log("mouse inside div");
+})
